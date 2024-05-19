50c9c5781b01c3106bbbf36267e50edadce620e5
lolfeeg <golubkov.dog@yandex.ru>
2024-05-19 21:43:42 +0400

    Update task1.py

--- task1/task1.py
+++ task1/task1.py
@@ -1,1 +1,13 @@
-
+from itertools import cycle
+n, m = int(input()), int(input())
+sk = cycle(range(1, n+1))
+def func(it, num):
+    left = next(it)
+    right = 0
+    while right != 1:
+        yield left
+        for _ in range(num-2):
+            next(it)
+        right = next(it)
+        left = right
+print(*func(sk, m), sep='')
