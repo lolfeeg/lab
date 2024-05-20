b603c5481e160968422bfb98994f923649bc855e
lolfeeg <golubkov.dog@yandex.ru>
2024-05-20 14:52:56 +0400

    Update task4.py

--- task4/task4.py
+++ task4/task4.py
@@ -1,1 +1,6 @@
-
+from pathlib import Path
+numbers = Path(input())
+with open(numbers, 'r', encoding='utf-8') as file:
+    sk = [int(cyc) for cyc in file]
+mid = round(sum(sk) / len(sk))
+print(sum(map(lambda x: abs(x - mid), sk)))
